2022a95e0a1669ac8d0be375bf8cb0ba261995c0
John Törnblom <john.tornblom@gmail.com>
2025-10-27 19:40:36 +0100

    cosmetics

--- main_ps4.c
+++ main_ps4.c
@@ -14,7 +14,6 @@ You should have received a copy of the GNU General Public License
 along with this program; see the file COPYING. If not, see
 <http://www.gnu.org/licenses/>.  */
 
-
 #include <signal.h>
 #include <stdlib.h>
 #include <string.h>
