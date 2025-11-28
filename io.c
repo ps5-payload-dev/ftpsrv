abea377f588f2043466895142cef2938a88c8407
John Törnblom <john.tornblom@gmail.com>
2025-11-28 10:58:12 +0100

    self2elf: speed improvements

--- io.c
+++ io.c
@@ -15,6 +15,7 @@ along with this program; see the file COPYING. If not, see
 <http://www.gnu.org/licenses/>.  */
 
 #include <errno.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 #include <sys/errno.h>
@@ -119,19 +120,25 @@ io_pwrite(int fd, const void* buf, size_t n, off_t off) {
 int
 io_pcopy(int fd_in, int fd_out, off_t off_in, off_t off_out, size_t size) {
   size_t copied = 0;
-  char buf[0x4000];
+  void* buf;
   ssize_t n;
+
+  if(!(buf=malloc(IO_COPY_BUFSIZE))) {
+    return -1;
+  }
 
   while(copied < size) {
     n = size - copied;
-    if(n > sizeof(buf)) {
-      n = sizeof(buf);
+    if(n > IO_COPY_BUFSIZE) {
+      n = IO_COPY_BUFSIZE;
     }
 
     if(io_pread(fd_in, buf, n, off_in)) {
+      free(buf);
       return -1;
     }
     if(io_pwrite(fd_out, buf, n, off_out)) {
+      free(buf);
       return -1;
     }
 
@@ -140,6 +147,7 @@ io_pcopy(int fd_in, int fd_out, off_t off_in, off_t off_out, size_t size) {
     copied += n;
   }
 
+  free(buf);
   return 0;
 }
 
