c3a6fd253b6620394f9be4d5e3646936bdf597ab
John Törnblom <john.tornblom@gmail.com>
2026-01-09 15:40:16 +0100

    test: initial commit

---
+++ test/gen-testdata.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+LARGE_NUMBER=10000
+SMALL_NUMBER=1
+
+
+echo "testdata1: large number of folders with a single small text file"
+rm -rf testdata1
+mkdir -p testdata1
+for i in $(seq 1 $LARGE_NUMBER); do
+    mkdir -p "testdata1/folder$i"
+    echo "hello, world" > "testdata1/folder$i/hello_world.txt"
+done
+
+
+echo "testdata2: single folder with a large number of small text files"
+rm -rf testdata2
+mkdir -p testdata2
+for i in $(seq 1 $LARGE_NUMBER); do
+    echo "hello, world" > "testdata2/file$i.txt"
+done
+
+exit 0
+
+echo "testdata3: large number of folders with a large number of small text files"
+rm -rf testdata3
+mkdir -p testdata3
+for i in $(seq 1 $LARGE_NUMBER); do
+    mkdir -p "testdata3/folder$i"
+    for j in $(seq 1 $LARGE_NUMBER); do
+	echo "hello, world" > "testdata3/folder$i/file$j.txt"
+    done
+done
+
+echo "testdata4: a single large binary file"
+rm -rf testdata4
+mkdir -p testdata4
+head -c 1G /dev/urandom > testdata4/1GB.bin
---
+++ test/test-lftp.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+if [ "$#" -lt 2 ]; then
+    echo "usage: $0 <HOST> <LOCAL_PATH> <REMOTE_PATH>"
+    exit 1
+fi
+
+HOST=$1
+LOCAL_PATH=$2
+REMOTE_PATH=$3
+PORT=2121
+THREADS=64
+
+TMP_PATH=$(mktemp -d)
+trap 'rm -rf -- "$TMP_PATH"' EXIT
+
+function test_upload() {
+    lftp -p "$PORT" "$HOST" <<EOF
+set ftp:ssl-allow no
+set mirror:parallel-transfer-count $THREADS
+mirror -R "$LOCAL_PATH" "$REMOTE_PATH"
+quit
+EOF
+}
+
+function test_download() {
+    lftp -p "$PORT" "$HOST" <<EOF
+set ftp:ssl-allow no
+set mirror:parallel-transfer-count $THREADS
+mirror "$REMOTE_PATH" "$TMP_PATH/$REMOTE_PATH"
+quit
+EOF
+}
+
+function test_delete() {
+    lftp -p "$PORT" "$HOST" <<EOF
+set ftp:ssl-allow no
+set mirror:parallel-transfer-count $THREADS
+rm -r "$REMOTE_PATH"
+quit
+EOF
+}
+
+UPLOAD_START=$(date +%s)
+test_upload
+UPLOAD_END=$(date +%s)
+UPLOAD_TIME=$((UPLOAD_END - UPLOAD_START))
+
+
+DOWNLOAD_START=$(date +%s)
+test_download
+DOWNLOAD_END=$(date +%s)
+DOWNLOAD_TIME=$((DOWNLOAD_END - DOWNLOAD_START))
+
+DELETE_START=$(date +%s)
+test_delete
+DELETE_END=$(date +%s)
+DELETE_TIME=$((DELETE_END - DELETE_START))
+
+echo ""
+echo "Upload time:   $UPLOAD_TIME sec"
+echo "Download time: $DOWNLOAD_TIME sec"
+echo "Delete time:   $DELETE_TIME sec"
